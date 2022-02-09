424bbc78f85fd875fb61f7b0ced27a5a49c4335b
rob.lascelles <rob.lascelles@just-eat.com>
2022-02-09 12:37:05 +0000

    extends payment processing web api test coverage

--- test/CheckoutChallenge.WebAPI.Tests/ProcessingTests.cs
+++ test/CheckoutChallenge.WebAPI.Tests/ProcessingTests.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
+using CheckoutChallenge.Application.PaymentProcessing;
 using CheckoutChallenge.WebAPI.Models;
 using Shouldly;
 using Xunit;
@@ -11,8 +13,8 @@ namespace CheckoutChallenge.WebAPI.Tests;
 public class ProcessingTests
 {
     [Theory]
-    [InlineData("merchant", "ref", 1299, "GBP", "2030-12", "123", "4111111111111111", "Mrs Test")]
-    public async Task ValidProcessPaymentRequestsReturn200(string merchantId, string merchantRef, int amount, string currency, string expiry, string cvv, string pan, string cardholderName)
+    [InlineData("ref", 1299, "GBP", "2030-12", "123", "4111111111111111", "Mrs Test")]
+    public async Task ValidProcessPaymentRequestsReturn200(string merchantRef, int amount, string currency, string expiry, string cvv, string pan, string cardholderName)
     {
         await using var application = new TestWebAPIHost();
 
@@ -34,6 +36,22 @@ public class ProcessingTests
         var response = await client.PostAsJsonAsync($"/api/payments", request);
         response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
-        //TODO: deserialise & assert on response contents
+        var json = await response.Content.ReadAsStringAsync();
+        var statusResponse = JsonSerializer.Deserialize<ProcessPaymentResponse>(json, CamelCase);
+
+        statusResponse.ShouldNotBeNull();
+
+        statusResponse.Currency.ShouldBe(currency);
+        statusResponse.Amount.ShouldBe(amount);
+        
+        statusResponse.MerchantRef.ShouldBe(merchantRef);
+        
+        statusResponse.Approved.ShouldBe(true);
+        statusResponse.Status.ShouldBe(PaymentStatus.Authorized.ToString());
+
+        statusResponse.AuthCode.ShouldNotBeNullOrEmpty();
+        statusResponse.PaymentId.ShouldNotBeNullOrEmpty();
     }
+
+    private static JsonSerializerOptions CamelCase => new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 }
